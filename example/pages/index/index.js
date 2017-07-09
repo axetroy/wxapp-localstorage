8ee07fa20e517bd454b3be74df18ce4983483dd0
axetroy <troy450409405@gmail.com>
2017-07-10 00:14:30 +0800

    docs: update example

--- example/pages/index/index.js
+++ example/pages/index/index.js
@@ -8,8 +8,28 @@ Page({
     success: 0,
     fail: 0
   },
+  set() {
+    wx.localStorage.setItem('test', Math.random());
+    this.setData({ test: wx.localStorage.test });
+  },
+  get() {
+    wx.showModal({
+      title: 'Current Value',
+      content: wx.localStorage.getItem('test') + ''
+    });
+  },
+  remove() {
+    wx.localStorage.removeItem('test');
+    console.log(wx.localStorage.test);
+    this.setData({ test: wx.localStorage.test || 'null' });
+  },
+  clear() {
+    wx.localStorage.clear();
+    this.setData({ test: wx.localStorage.test || 'null' });
+  },
   onLoad: function() {
-    wx.localStorage.setItem('random', Math.random());
+    this.set();
+    wx.localStorage.setItem('test', Math.random());
     console.log(wx.localStorage);
   }
 });
